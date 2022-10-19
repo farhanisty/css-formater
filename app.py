2eb7d067dcc6337dc4b1505d8ba3f9398129af4e
farhannivta_ramadhana <farhannivta_ramadhana@quick.com>
2022-10-19 10:20:03 +0700

    membuat base menu

--- app.py
+++ app.py
@@ -3,12 +3,60 @@ import os
 from CssFormater import CssFormater
 from Database import Database
 
-# g = Generator("mybootstrap")
 
-# g.write("hallo bung")
+class Main:
+    def __init__(self):
+        self.pwd = os.getcwd()
+        self.file = ""
+        self.page = ""
+        self.mainMenu()
+    
+    #=================MENUS===============#
+    #--------MAIN MENU----------#
+    def mainMenu(self):
+        self.page = "menu"
+        print("\n----------CSS Generator by farhannivta ramadhana------------")
+        self._createMenu({"choose file" : self.setFile, "quit" : self.quit})
 
-d = Database("css_generator_db")
+    def _createMenu(self, list):
+        self._printInfo()
+        lst = list
+        klst = [key for key, val in lst.items()]
 
-data = d.select().getOne()
+        no = 1
+        for i in lst:
+            print(f"{no}. {klst[no - 1]}")
+            no += 1
+        ipt = int(input("input anda: "))
 
-print(data)
+        if ipt > len(klst) or ipt <= 0:
+            print("bego")
+            self._createMenu(list)
+        lst[klst[ipt-1]]()
+
+    def _printInfo(self):
+        self._printSpace(f"pwd :{self.pwd}")
+        if self.file == "":
+            self._printSpaceEnd(f"file :========Not set yet==========")
+        else:
+            self._printSpaceEnd(f"file :{self.file}")
+    
+    def _printSpace(self, msg, n = 1):
+        print("\n" * n + msg)
+
+    def _printSpaceEnd(self, msg, n = 1):
+        print(msg + "\n" * n)
+
+
+
+    #================FUNCTIONS=================#
+    #---------MENU FUNCTIONS------------#
+    def setFile(self):
+        print("hello world")
+
+    def quit(self):
+        print("bye")
+    
+
+if __name__ == "__main__":
+    Main()
